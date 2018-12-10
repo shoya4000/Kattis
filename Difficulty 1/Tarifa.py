daedcb94a5a433aab4ad80122e957108c1d0a8be
Sho Ya <shoya@divdot.com>
2018-12-09 16:03:24 -0800

    2 more trivial

---
+++ Difficulty 1/Tarifa.py
@@ -0,0 +1,55 @@
+# Tarifa
+# Pero has negotiated a Very Good data plan with his internet provider. The provider will let Pero use up X megabytes to surf the internet per month. Each megabyte that he doesn’t spend in that month gets transferred to the next month and can still be spent. Of course, Pero can only spend the megabytes he actually has.
+# If we know how much megabytes Pero has spent in each of the first N
+# months of using the plan, determine how many megabytes Pero will have
+# available in the N+1 month of using the plan.
+
+# Input
+# The first line of input contains the integer X (1≤X≤100). The second
+# line of input contains the integer N (1≤N≤100). Each of the following N
+# lines contains an integer Pi (0≤Pi≤10000), the number of megabytes spent
+# in each of the first N months of using the plan. Numbers Pi will be such
+# that Pero will never use more megabytes than he actually has.
+
+# Output
+# The first and only line of output must contain the required value from
+# the task.
+
+# Sample Input 1 	Sample Output 1
+# 10					28
+# 3
+# 4
+# 6
+# 2
+
+# Sample Input 2 	Sample Output 2
+# 10					16
+# 3
+# 10
+# 2
+# 12
+
+# Sample Input 3 	Sample Output 3
+# 15					15
+# 3
+# 15
+# 10
+# 20
+
+# Solution accepted: 2018-12-09 15:48:55
+
+import sys
+import math
+
+
+def main(input):
+    x = int(input.readline())
+    n = int(input.readline())
+    limit = x * (n + 1)
+    for i in range(n):
+        usage = int(input.readline())
+        limit -= usage
+    print(limit)
+
+if __name__ == '__main__':
+    main(sys.stdin)
---
+++ Difficulty 1/Timeloop.py
@@ -0,0 +1,46 @@
+# Stuck In A Time Loop
+# Last night when you went to sleep, you had a strange feeling that you may see the same day again. And your strange feeling came to be when you woke up, everyone seemed to think that it was yesterday morning! The same strange feeling came back in the evening.
+# When this pattern continued for days, you looked for help from a time
+# wizard. The wizard says he can break you out of the time loop, but you
+# must chant his spell. The wizard gives you a magic number, and you must
+# count up to that number, starting at 1, saying “Abracadabra” each time.
+
+# Input
+# Input consists of a single integer N (1≤N≤100).
+
+# Output
+# Output the entire wizard’s spell by counting from 1 to N, giving one
+# number and “Abracadabra” per line.
+
+# Sample Input 1 	Sample Output 1
+# 5					1 Abracadabra
+# 					2 Abracadabra
+# 					3 Abracadabra
+# 					4 Abracadabra
+# 					5 Abracadabra
+
+# Sample Input 2 	Sample Output 2
+# 10				1 Abracadabra
+# 					2 Abracadabra
+# 					3 Abracadabra
+# 					4 Abracadabra
+# 					5 Abracadabra
+# 					6 Abracadabra
+# 					7 Abracadabra
+# 					8 Abracadabra
+# 					9 Abracadabra
+# 					10 Abracadabra
+
+# Solution accepted: 2018-12-09 16:02:48
+
+import sys
+import math
+
+
+def main(input):
+    n = int(input.readline())
+    for i in range(n):
+        print(str(i + 1) + " Abracadabra")
+
+if __name__ == '__main__':
+    main(sys.stdin)
