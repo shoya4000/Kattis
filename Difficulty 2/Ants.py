b654dab091bdfb15182728afcd6860c45ae2a0dc
Shō Ya Voorthuyzen <mikevoorthuyzen@hotmail.com>
2019-09-20 17:28:51 -0700

    Create Ants.py

---
+++ Difficulty 2/Ants.py
@@ -0,0 +1,18 @@
+# Ants
+# An army of ants walk on a horizontal pole of length l cm, each with a constant speed of 1 cm/s. When a walking ant reaches an end of the pole, it immediatelly falls off it. When two ants meet they turn back and start walking in opposite directions. We know the original positions of ants on the pole, unfortunately, we do not know the directions in which the ants are walking. Your task is to compute the earliest and the latest possible times needed for all ants to fall off the pole.
+
+# Input
+# The first line of input contains one integer giving the number of cases that follow, at most 100. The data for each case start with two integer numbers: the length l of the pole (in cm) and n, the number of ants residing on the pole. These two numbers are followed by n integers giving the position of each ant on the pole as the distance measured from the left end of the pole, in no particular order. All input integers are between 0 and 1000000 and they are separated by whitespace.
+
+# Output
+# For each case of input, output two numbers separated by a single space. The first number is the earliest possible time when all ants fall off the pole (if the directions of their walks are chosen appropriately) and the second number is the latest possible such time.
+
+# Solution accepted:
+
+import sys
+
+def main(input):
+    print("Hello World")
+
+if __name__ == '__main__':
+    main(sys.stdin)
